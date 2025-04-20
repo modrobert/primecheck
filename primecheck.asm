ed1b1708209fb60c91a3543efa897cdb922bd89f
modrobert <modrobert@gmail.com>
2025-04-20 15:26:33 +0700

    fixed tabs

--- primecheck.asm
+++ primecheck.asm
@@ -17,18 +17,18 @@
 ;
 
 ; primechk(unsigned long int) returns 1 if input (rax) is a prime,
-; otherwise 0; 
-; 
-; 386 opcodes with 64bit registers for x86-64 (AMD64)
-; 
+; otherwise 0;
 ;
-; Optimization log: 
+; 386 opcodes with 64bit registers for x86-64 (AMD64)
+;
+;
+; Optimization log:
 ;
 ; 2002-02-19: All primes except 2 always have bit 0 set, always an odd value.
 ;
 ; 2002-02-19: No need to check multiples higher than square root of input.
 ;
-; 2002-02-23: Square root is done quicker by using "multiply by self" inside 
+; 2002-02-23: Square root is done quicker by using "multiply by self" inside
 ;             primeloop instead of fsqrt before loop.
 ;
 ; 2002-02-23: Check for low primes before primeloop, not within.
@@ -44,10 +44,10 @@
 ;     floats/doubles: XMM0 to XMM7
 ;
 ; The first six integer arguments (from the left) are passed in
-; RDI, RSI, RDX, RCX, R8, and R9, in that order. 
-; Additional integer arguments are passed on the stack. 
+; RDI, RSI, RDX, RCX, R8, and R9, in that order.
+; Additional integer arguments are passed on the stack.
 ; These registers, plus RAX, R10 and R11 are destroyed by function calls,
-; and thus are available for use by the function without saving. 
+; and thus are available for use by the function without saving.
 
 
 		BITS 64
@@ -58,10 +58,10 @@ primechk:
 		mov r9,rbx		;save rbx since we use it
 		mov rcx,rdi		;get our 64bit "prime suspect" ;)
 
-; Eliminate low primes first, since square root rounded up don't work when 
+; Eliminate low primes first, since square root rounded up don't work when
 ; rax is less or equal to 5. Also save some important clocks in primeloop.
 
-		cmp rcx,0x1		
+		cmp rcx,0x1
 		je noprime
 		cmp rcx,0x2
 		je prime
@@ -77,8 +77,7 @@ primechk:
 		bt rcx,0		;check for bit 0 (0-63)
 		jnc noprime		;not set? not odd, no prime, exit
 
-	
-; Now eliminate numbers that aren't primes by dividing with all odd multiples 
+; Now eliminate numbers that aren't primes by dividing with all odd multiples
 ; up to square root of input (rax).
 
 initprime:
@@ -89,7 +88,7 @@ primeloop:
 		mul rbx			;multiply by self
 		jo prime		;if overflow is set, we got a prime
 		cmp rax,rcx		;check result > square root?
-		ja prime		;ok, we got a prime 
+		ja prime		;ok, we got a prime
 		cmp rbx,rcx		;we finished dividing?
 		jae prime		;yes, we got a prime
 		mov rax,rcx		;preparing for divide
@@ -104,11 +103,10 @@ noprime:
 		mov rax,0x0		;return 0, no prime
 		jmp finito
 
-prime:	
+prime:
 		mov rax,0x1		;return 1 if prime
-		jmp finito 
+		jmp finito
 
-finito:	
+finito:
 		mov rbx,r9		;restore rbx since we used it
-		ret				;return to caller
-
+		ret 			;return to caller
